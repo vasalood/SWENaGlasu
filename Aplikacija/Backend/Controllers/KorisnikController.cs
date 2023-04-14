18142dfa600f08476fbb83ca26445d7c25c7b931
VVeljovic <veljkoveljovic13@gmail.com>
2023-04-14 13:26:31 +0200

    KorisnikStart

---
+++ Aplikacija/Backend/Controllers/KorisnikController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class KorisnikController : ControllerBase
+{
+    public NaGlasuContext Context { get; set; }
+    public KorisnikController(NaGlasuContext context)
+    {
+        Context=context;
+    }
+    [Route("Preuzmi")]
+    [HttpGet]
+    public ActionResult Preuzmi()
+    {
+        return Ok(Context.Korisnici);
+    }
+    [Route("Registracija")]
+    [HttpPost]
+    public async Task<ActionResult> Registracija([FromBody] Korisnik korisnik)
+    {
+        var postojeciKorisnik = Context.Korisnici.FirstOrDefault(kor=>kor.Email==korisnik.Email);
+        if (postojeciKorisnik!= null)
+        {
+            return BadRequest("Email već postoji");
+        }
+        if(string.IsNullOrWhiteSpace(korisnik.Ime)||korisnik.Ime.Length>50)
+        {
+            return BadRequest("Pogresno ime");
+        }
+         if(string.IsNullOrWhiteSpace(korisnik.Prezime)||korisnik.Prezime.Length>50)
+        {
+            return BadRequest("Pogresno prezime");
+        }
+        try
+        {
+            Context.Korisnici.Add(korisnik);
+            await Context.SaveChangesAsync();
+            return Ok("Uspesna registracija!");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
---
+++ Aplikacija/Backend/Models/Korisnik/Korisnik.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Models
+{
+    [Table("Korisnik")]
+    public class Korisnik
+    {
+        [Key]
+        
+         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Ime { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Prezime { get; set; }
+        [Required]
+        public int Telefon { get; set; }
+        public string Sifra { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Molimo unesite validnu email adresu.")]
+        public string Email { get; set; }
+
+    }
+}
