0cae6d03e019ceec9d977c41804cb6fde0dee896
Uros_M_Elfak <uros.marinkovic@elfak.rs>
2023-05-05 00:15:47 +0200

    Osnova za sve order operacije napravljena da radi

--- Aplikacija/Backend/WorkingDir/Utility/OrderBy.cs
+++ Aplikacija/Backend/WorkingDir/Utility/OrderBy.cs
@@ -1,6 +1,11 @@
 namespace Utility;
 
-public enum OrderBy
+public static class OrderBy
 {
-    Alphabet,Date,Price,Popularnost
+    public readonly static string NAZIV = "naziv";
+    public readonly static string CENA = "cena";
+    public readonly static string POPULARNOST = "popularnost";
+    public readonly static string DATUM = "datum";
+
+    public readonly static string UKUPNA_CENA = "ukupna_cena";
 }
--- Aplikacija/Backend/WorkingDir/Utility/OrderByMapper.cs
+++
@@ -1,8 +0,0 @@
-using System.Linq.Expressions;
-
-namespace Utility;
-
-public abstract class OrderByMapper<T> 
-{
-    public abstract Expression<Func<T,object>> Map(OrderBy orderBy);
-}
