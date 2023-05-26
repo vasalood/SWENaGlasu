937388d707864954ea1a506568cc3f0830fb6c08
Vasilije Tomovic <vasilije.tomovic@elfak.rs>
2023-05-26 21:13:24 +0200

    Prikaz oglasa

--- Aplikacija/Frontend/naglasu/src/Vasa/Komponente/Oglas.jsx
+++ Aplikacija/Frontend/naglasu/src/Vasa/Komponente/Oglas.jsx
@@ -1,19 +1,61 @@
+import { useState } from "react";
 import bicikla1 from "./Hardkodirani podaci/bicikla1.jpeg";
 import bicikla2 from "./Hardkodirani podaci/bicikla2.jpeg";
 import bicikla3 from "./Hardkodirani podaci/bicikla3.jpeg";
 import bicikla4 from "./Hardkodirani podaci/bicikla4.jpeg";
 import bicikla5 from "./Hardkodirani podaci/bicikla5.jpeg";
-
+import data from "./PodaciOglas.json";
 
 export default function Oglas() {
 
   const nizSlika = [bicikla1, bicikla2, bicikla3, bicikla4, bicikla5]
 
+  const [dugmePrati, setDugmePrati] = useState('Prati oglas');
+  const [slika, setSlika] = useState(0);
+
+  function napraviDatum(datumJson) {
+    const datum = new Date(datumJson);
+    let dan = datum.getDate();
+    let mesec = datum.getMonth() + 1;
+    const godina = datum.getFullYear();
+
+    if(dan < 10) dan = `0${dan}`;
+    if(mesec < 10) mesec = `0${mesec}`;
+
+    return `${dan}.${mesec}.${godina}.`;
+  }
+
+  function handleDugmePrati() {
+    setDugmePrati(prevDugmePrati => {
+      let novo;
+      if(prevDugmePrati === 'Prati oglas') {
+        novo = `Ukupan broj pratilaca: ${data.brojPregleda}`;
+      } else {
+        novo = 'Prati oglas';
+      }
+      return novo;
+    })
+  }
+
+  function handleSlikaLevo() {
+    setSlika(prevSlika => {
+      if(prevSlika === 0) return nizSlika.length - 1;
+      else return prevSlika - 1;
+    })
+  }
+  
+  function handleSlikaDesno() {
+    setSlika(prevSlika => {
+      if(prevSlika === nizSlika.length - 1) return 0;
+      else return prevSlika + 1
+    })
+  }
+
   return (
     <>
       <div className="oglas">
         <div className="oglas-header">
-          <h1>Bicikla na prodaju</h1>
+          <h1>{data.ime}</h1>
           <button className="button-prvi-tip">Postavi oglas</button>
         </div>
         <hr></hr>
@@ -27,26 +69,26 @@ export default function Oglas() {
 
             <div className="oglas-informacije-red oglas-informacije-cena">
               <label>Cena:</label>
-              <label>3000</label>
+              <label>{data.cena}</label>
             </div>
 
             <div className="oglas-informacije-red">
               <label>Kolicina:</label>
-              <label>1</label>
+              <label>{data.kolicina}</label>
             </div>
 
             <div className="oglas-informacije-red">
               <label>Datum postavljanja:</label>
-              <label>10.12.2022.</label>
+              <label>{napraviDatum(data.datumPostavljanja)}</label>
             </div>
 
             <div className="oglas-informacije-red">
               <label>Stanje:</label>
-              <label>Polovno</label>
+              <label>{data.stanje}</label>
             </div>
 
             <div className="oglas-informacije-red">
-              <button>Prati oglas</button>
+              <button onClick={handleDugmePrati}>{dugmePrati}</button>
             </div>
 
           </div>
@@ -55,7 +97,7 @@ export default function Oglas() {
 
             <div className="oglas-informacije-red">
               <label>Korisnik:</label>
-              <label>Milovan</label>
+              <label>{data.vlasnikKorisnickoIme}</label>
             </div>
 
             <div className="oglas-informacije-red">
@@ -64,10 +106,11 @@ export default function Oglas() {
             
             <div className="oglas-informacije-red">
               <label>Lokacija:</label>
-              <label>Nis</label>
+              <label>{data.lokacija}</label>
             </div>
 
             <div className="oglas-informacije-red">
+              {/*odakle se racuna ocena?*/}
               <label>Ocena</label>
               <label>4.5</label>
             </div>
@@ -83,11 +126,14 @@ export default function Oglas() {
 
         <div className="oglas-pet-slika">
 
-            <button> {'<'} </button>
-            <img src={nizSlika[0]} className="oglas-slika" />
-            <button> {'>'}</button>
+            <button onClick={handleSlikaLevo}> {'<'} </button>
+            <img src={nizSlika[slika]} className="oglas-slika" />
+            <button onClick={handleSlikaDesno}> {'>'}</button>
 
         </div>
+        
+        {/*polja? opis?*/}
+
       </div>
     </>
   )
